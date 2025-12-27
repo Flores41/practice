5593b65f1665762f502a3165387b199b48595cd5
Flores41 <jose_flores28@usmp.pe>
2025-12-27 11:12:00 -0500

    feat(presentation): add Riverpod providers for pokemon feature

--- lib/core/network/graphql_config.dart
+++ lib/core/network/graphql_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-final graphqlClientProvider = Provider<GraphQLClient>((ref) {
+final graphQLClientProvider = Provider<GraphQLClient>((ref) {
   final HttpLink httpLink = HttpLink('https://graphql-pokeapi.graphcdn.app/');
 
   return GraphQLClient(
